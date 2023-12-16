bd959ab8d554d17e4e8f263277e9a1714a3dab2f
Eduardo Ortega <5791035+ejortegau@users.noreply.github.com>
2023-12-16 20:47:40 +0100

    More fixes to controller reset on start-up

--- assets/unifi_entrypoint.sh
+++ assets/unifi_entrypoint.sh
@@ -2,13 +2,16 @@
 
 OUTPUT="/var/lib/unifi/system.properties"
 
-echo "Creating ${OUTPUT} with external DB configuration"
-{
-echo "db.mongo.local=false"
-echo "db.mongo.uri=mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}"
-echo "statdb.mongo.uri=mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}_stat"
-echo "unifi.db.name=${MONGO_DBNAME}"
-echo "is_default=false"
-} > "${OUTPUT}"
+
+if [[ ! -f "${OUTPUT}" ]]; then
+  echo "Creating ${OUTPUT} with external DB configuration"
+  {
+  echo "db.mongo.local=false"
+  echo "db.mongo.uri=mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}"
+  echo "statdb.mongo.uri=mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}_stat"
+  echo "unifi.db.name=${MONGO_DBNAME}"
+  } > "${OUTPUT}"
+fi
+
 
 java -Xms1024M -Xmx1024M -Dlog4j2.formatMsgNoLookups=true --add-opens java.base/java.time=ALL-UNNAMED -jar /usr/lib/unifi/lib/ace.jar start
